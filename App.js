158f25dbd963d7dbcb4ce9ec59e7db5cf537f2d1
jr-cheung <54596609+jr-cheung@users.noreply.github.com>
2020-07-20 16:14:17 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import ReactDOM from "react-dom";
+import ReactTooltip from "react-tooltip";
+
+import './App.css';
+
+import MapChart from "./MapChart";
+
+function App() {
+	const [content, setContent] = useState("");
+	return (
+	  <div>
+		<MapChart setTooltipContent={setContent} />
+		<ReactTooltip>{content}</ReactTooltip>
+	  </div>
+	);
+  }
+  
+  const rootElement = document.getElementById("root");
+  ReactDOM.render(<App />, rootElement);
+
+export default App;
